d5b61b0762508a34e2c4a92be4fd08b23ed207e1
dm1try <me@dmitry.it>
2018-03-22 09:41:25 +0300

    use stub_const to prevent its leakage

--- spec/runner_spec.rb
+++ spec/runner_spec.rb
@@ -1,7 +1,8 @@
 require 'bjob/runner'
 
 RSpec.describe BJob::Runner do
-  let(:job_const) { DumbJob = Class.new }
+  let(:job_const) { 'DumbJob' }
+  let(:job_class) { stub_const(job_const, Class.new) }
   let(:job_method) { 'do_it' }
   let(:job_params) { [:some_param_value, :other_param] }
   let(:job_result) { 'done' }
@@ -10,11 +11,11 @@ RSpec.describe BJob::Runner do
 
   context 'job implementation exists in the system' do
     before do
-      allow_any_instance_of(job_const).to receive(job_method).with(*job_params).and_return(job_result)
+      allow_any_instance_of(job_class).to receive(job_method).with(*job_params).and_return(job_result)
     end
 
     it 'creates a job instance, run it and returns the call result' do
-      expect_any_instance_of(job_const).to receive(job_method).with(*job_params)
+      expect_any_instance_of(job_class).to receive(job_method).with(*job_params)
       expect(subject.run(job)).to eq(job_result)
     end
   end
