136d17de63c9c50f1d509ee85ab26af03cd5b034
dm1try <me@dmitry.it>
2018-03-22 04:13:22 +0300

    run pool for doing work

--- lib/bjob/coordinator.rb
+++ lib/bjob/coordinator.rb
@@ -1,5 +1,20 @@
 module BJob
   class Coordinator
+    def initialize(pool_size: 16)
+      @running_queue = Queue.new
+      @pool_size = pool_size
+    end
+
+    def start
+      @pool_size.times do
+        Thread.new do
+          loop do
+            @running_queue.pop
+          end
+        end
+      end
+    end
+
     def schedule(job)
       :ok
     end
--- spec/coordinator_spec.rb
+++ spec/coordinator_spec.rb
@@ -3,6 +3,12 @@ require 'bjob/coordinator'
 RSpec.describe BJob::Coordinator do
   let(:job) { {some: :job} }
 
+  describe '#start' do
+    it 'runs a pool of threads for doing work' do
+      expect { subject.start }.to change{ Thread.list.count }.by(16)
+    end
+  end
+
   it 'schedules a job' do
     expect(subject.schedule(job)).to eq(:ok)
   end
