63a1628e90da5d5f884ecaa6642feb77bd31ebad
dm1try <me@dmitry.it>
2018-03-22 08:14:55 +0300

    use explicit job pool size in spec

--- spec/coordinator_spec.rb
+++ spec/coordinator_spec.rb
@@ -3,19 +3,20 @@ require 'bjob/test/runner'
 
 RSpec.describe BJob::Coordinator do
   let(:job) { {'class' => 'SomeJob', 'method' => 'run', 'params' => [] } }
+  let(:jobs_pool_size) { 4 }
 
-  subject { described_class.new(runner: BJob::Test::Runner) }
+  subject { described_class.new(runner: BJob::Test::Runner, pool_size: jobs_pool_size) }
 
   describe '#start' do
     it 'runs a pool of threads for doing work' do
-      expect { subject.start }.to change{ Thread.list.count }.by(16)
+      expect { subject.start }.to change{ Thread.list.count }.by(jobs_pool_size)
     end
   end
 
   describe '#stop' do
     it 'shutdown workers pool' do
       subject.start
-      expect { subject.stop }.to change{ Thread.list.count }.by(-16)
+      expect { subject.stop }.to change{ Thread.list.count }.by(-(jobs_pool_size))
     end
   end
 
