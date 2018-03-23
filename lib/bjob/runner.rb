056f22d1bd4bf5ad7d181ab0c4e790d2a2d92acc
dm1try <me@dmitry.it>
2018-03-23 14:00:26 +0300

    namespace synatax

--- lib/bjob/runner.rb
+++ lib/bjob/runner.rb
@@ -1,37 +1,38 @@
-class BJob::Runner
-
-  def initialize(logger: BJob.logger)
-    @logger = logger
-  end
-
-  def run(job)
-    @logger.info("job ##{job['id']} started")
-    start_time = Time.now
-
-    job_const =
-      begin
-        Object.const_get(job['class'])
-      rescue NameError
-        @logger.warn("missing constant #{job['class']}, skipping job...")
-        return
-      end
-
-    method = job['method']
-    params = job['params']
-
-    begin
-      result = job_const.new.send(method, *params)
-      @logger.info("job ##{job['id']} done: #{elapsed_time(start_time)} ms")
-      result
-    rescue StandardError => error
-      @logger.error("job ##{job['id']} failed: #{error}")
-      nil
+module BJob
+  class Runner
+    def initialize(logger: BJob.logger)
+      @logger = logger
     end
-  end
 
-  private
+    def run(job)
+      @logger.info("job ##{job['id']} started")
+      start_time = Time.now
 
-  def elapsed_time(start_time)
-    Time.now - start_time
+      job_const =
+        begin
+          Object.const_get(job['class'])
+        rescue NameError
+          @logger.warn("missing constant #{job['class']}, skipping job...")
+          return
+        end
+
+      method = job['method']
+      params = job['params']
+
+      begin
+        result = job_const.new.send(method, *params)
+        @logger.info("job ##{job['id']} done: #{elapsed_time(start_time)} ms")
+        result
+      rescue StandardError => error
+        @logger.error("job ##{job['id']} failed: #{error}")
+        nil
+      end
+    end
+
+    private
+
+    def elapsed_time(start_time)
+      Time.now - start_time
+    end
   end
 end
