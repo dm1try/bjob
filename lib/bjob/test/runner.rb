1b734b9fc06608c34ab7ebcc77a0279b6b476190
dm1try <me@dmitry.it>
2018-03-23 13:44:45 +0300

    avoid different namespacing syntax

--- lib/bjob/test/runner.rb
+++ lib/bjob/test/runner.rb
@@ -1,11 +1,13 @@
-module BJob::Test
-  class Runner
-    @@jobs = []
-    def self.jobs; @@jobs end
-    def self.reset; @@jobs = [] end
+module BJob
+  module Test
+    class Runner
+      @@jobs = []
+      def self.jobs; @@jobs end
+      def self.reset; @@jobs = [] end
 
-    def run(job)
-      @@jobs << job
+      def run(job)
+        @@jobs << job
+      end
     end
   end
 end
