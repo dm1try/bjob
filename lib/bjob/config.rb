f23c3178b0f224cc6c892007d7e6f3d4c9d47618
dm1try <me@dmitry.it>
2018-03-25 19:05:56 +0300

    add ability to load config based on command line options

--- lib/bjob/config.rb
+++ lib/bjob/config.rb
@@ -1,4 +1,5 @@
 require 'ostruct'
+require 'optparse'
 
 module BJob
   class Config
@@ -10,6 +11,28 @@ module BJob
         config.saved_jobs_path = ENV['BJOB_SAVED_JOBS_PATH'] || '/tmp/bjob_saved_jobs'
         config
       end
+    end
+
+    def self.from_argv
+      config = default.dup
+
+      OptionParser.new do |opts|
+        opts.banner = "Usage: bjob [options]"
+
+        opts.on("-c", "--concurrency COUNT", Integer, "number of threads used for processing jobs") do |v|
+          config.concurrency = v
+        end
+
+        opts.on("", "--unix-socket-path UNIX_SOCKET_PATH", "socket path for listening for job requests") do |v|
+          config.unix_socket_path = v
+        end
+
+        opts.on("", "--saved-jobs-path SAVED_JOBS_PATH", "path for storing unfinished jobs") do |v|
+          config.saved_jobs_path = v
+        end
+      end.parse!
+
+      config
     end
   end
 end
--- spec/config_spec.rb
+++ spec/config_spec.rb
@@ -10,4 +10,20 @@ RSpec.describe BJob::Config do
       expect(config.saved_jobs_path).to be
     end
   end
+
+  describe '.from_argv' do
+    let(:concurrency) { 4 }
+    let(:unix_socket_path) { '/tmp/bjob_socket_from_command_line' }
+    let(:saved_jobs_path) { '/tmp/bjob_saved_jobs_path_from_command_line' }
+    let(:fake_argv) {  "-c #{concurrency} --unix-socket-path #{unix_socket_path} --saved-jobs-path #{saved_jobs_path}".split }
+
+    it 'return config based on command line options' do
+      stub_const('ARGV', fake_argv)
+
+      config = BJob::Config.from_argv
+      expect(config.concurrency).to eq(concurrency)
+      expect(config.unix_socket_path).to eq(unix_socket_path)
+      expect(config.saved_jobs_path).to eq(saved_jobs_path)
+    end
+  end
 end
