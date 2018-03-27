200db7c6c8010d23d7728e29828dcb6d308e58a2
dm1try <me@dmitry.it>
2018-03-27 09:02:10 +0300

    add priority queue implementation based on binary heap

---
+++ lib/bjob/priority_queue.rb
@@ -0,0 +1,50 @@
+module BJob
+  class PriorityQueue
+    # based on binary heap
+    UNUSED_ITEM = nil
+
+    def initialize(comparator: nil)
+      @comparator = comparator || ->(first, second) { first <=> second }
+      @items = [UNUSED_ITEM]
+    end
+
+    def push(item)
+      @items << item
+      swim(@items.size - 1)
+    end
+
+    def pop
+      exchange(1, @items.size - 1) if @items.size > 2
+      max = @items.pop
+      sink(1) if @items.size > 2
+      max
+    end
+
+    private
+
+    def swim(k)
+      while (k > 1 && less(k/2, k))
+        exchange(k, k/2)
+        k = k/2
+      end
+    end
+
+    def sink(k)
+      while (2*k <= @items.size)
+        j = 2*k
+        j +=1 if ((j < @items.size - 1) && less(j, j+1))
+        break if !less(k, j)
+        exchange(k, j)
+        k = j
+      end
+    end
+
+    def less(first, second)
+      @comparator.call(@items[first], @items[second]) < 0
+    end
+
+    def exchange(first, second)
+      @items[first], @items[second] = @items[second], @items[first]
+    end
+  end
+end
---
+++ spec/priority_queue_spec.rb
@@ -0,0 +1,31 @@
+require 'bjob/priority_queue'
+
+RSpec.describe BJob::PriorityQueue do
+  context 'each item is a hash with "priority" key' do
+    let(:comparator) { ->(l,r) { l['priority'] <=> r['priority'] } }
+    let(:subject) { described_class.new(comparator: comparator) }
+
+    it 'pops items based on their priority' do
+      subject.push({'id' => 'low', 'priority' => 1})
+      subject.push({'id' => 'high', 'priority' => 5})
+      subject.push({'id' => 'default', 'priority' => 2})
+      subject.push({'id' => 'default2', 'priority' => 4})
+      subject.push({'id' => 'low2', 'priority' => 1})
+      subject.push({'id' => 'default2', 'priority' => 2})
+      subject.push({'id' => 'default2', 'priority' => 4})
+      subject.push({'id' => 'high2', 'priority' => 2})
+      subject.push({'id' => 'high2', 'priority' => 3})
+
+      expect(subject.pop).to eq({'id' => 'high', 'priority' => 5})
+      expect(subject.pop).to include({'priority' => 4})
+      expect(subject.pop).to include({'priority' => 4})
+      expect(subject.pop).to include({'priority' => 3})
+      expect(subject.pop).to include({'priority' => 2})
+      expect(subject.pop).to include({'priority' => 2})
+      expect(subject.pop).to include({'priority' => 2})
+      expect(subject.pop).to include({'priority' => 1})
+      expect(subject.pop).to include({'priority' => 1})
+      expect(subject.pop).to be_nil
+    end
+  end
+end
